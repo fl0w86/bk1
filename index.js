e25aed320b79cc9471e3671a38582995293c1f11
Florian Kuc <florian.kuc@mailbox.org>
2019-11-17 17:47:58 +0100

    vor auth

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
 //ROUTE HANDLER
-app.get('/', (req, res) => {
-    res.send({ hi: 'there yo' })
-})
+passport.use(new GoogleStrategy());
 
 //IF HEROKU SETS PORT, USE IT, OTHERWISE USE 5000
 const PORT = process.env.PORT || 5000;
