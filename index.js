7112e45ecc2fe0e6427bd06d7d0de5a55825ad83
Florian Kuc <florian.kuc@mailbox.org>
2019-11-17 18:36:08 +0100

    auth phase 1 done

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 //ROUTE HANDLER
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback' //HANDLING AUTH ROUTE
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 //IF HEROKU SETS PORT, USE IT, OTHERWISE USE 5000
 const PORT = process.env.PORT || 5000;
