c6e9249f183bef721fceec1e70f58857d1477df3
Florian Kuc <florian.kuc@mailbox.org>
2019-11-21 12:44:23 +0100

    before heroku deployment

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+//ROUTE HANDLING IN PROD
+if (process.env.NODE_ENV === 'production') {
+    //EXPRESS WILL SERVE ASSETS LIKE MAIN.JS
+    app.use(express.static('client/build'));
+    //EXPRESS SERVES INDEX.HTML IF ROUTE UNKNOWN
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 //IF HEROKU SETS PORT, USE IT, OTHERWISE USE 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
